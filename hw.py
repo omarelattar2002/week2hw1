91751760adef06e127171a125eb6e7b0d2863cec
omar elattar <omarelattar2002@gmail.com>
2024-04-19 11:21:15 -0500

    done

--- hw.py
+++ hw.py
@@ -9,7 +9,21 @@ for cubednumbers in Numbertest:
 
 
 #execise 2
-n = range(1,100)
+# HINT::
+# An else after an if runs if the if didn’t
+# An else after a for runs if the for didn’t break
+
+
+
+# Prime number - A number that is divisible by only 1 and itself
+
+for n in range(1,100):
+    for i in range(2,n):
+        if n % i == 0:
+            break
+    else:
+        print(n)
+
 
 
 
